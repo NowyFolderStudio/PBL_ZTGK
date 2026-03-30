eea08a97c07417810b3a7d6c54ddd9bb0f085f19
rdzierzbicki <robert.dzierzbicki@gmail.com>
2026-03-30 21:37:09 +0200

    [PBL-11] Implement core UI architecture

---
+++ include/UI/UIComponents.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <memory>
+#include "Core/Texture.hpp"
+
+namespace NFSEngine {
+
+	struct UIComponent {
+		virtual ~UIComponent() = default;
+	};
+
+	struct RectTransform : public UIComponent {
+		glm::vec3 Position = glm::vec3(0.0f);
+
+		float Width = 100.0f;
+		float Height = 100.0f;
+		float Rotation = 0.0f;
+
+		glm::mat4 GetTransform() const {
+			glm::mat4 transform = glm::mat4(1.0f);
+
+			transform = glm::translate(transform, Position);
+			transform = glm::rotate(transform, glm::radians(Rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+			transform = glm::scale(transform, glm::vec3(Width, Height, 1.0f));
+
+			return transform;
+		}
+	};
+
+	struct ImageComponent : public UIComponent {
+		glm::vec4 Color = glm::vec4(1.0f);
+
+		std::shared_ptr<Texture> TexturePtr = nullptr;
+
+		ImageComponent() = default;
+		ImageComponent(const glm::vec4& color) : Color(color) {}
+		ImageComponent(const std::shared_ptr<Texture>& texture, const glm::vec4& tintColor = glm::vec4(1.0f)) : TexturePtr(texture), Color(tintColor) {}
+	};
+}
