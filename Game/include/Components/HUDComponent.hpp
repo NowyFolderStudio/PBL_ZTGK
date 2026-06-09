5b2431f1afbbbc9e3fc2df726ea911bf1d60bd6a
Dawid Gradowski <d.gradowski23@gmail.com>
2026-06-10 00:36:29 +0200

    [PBL-102] Add new score UI element

--- Game/include/Components/HUDComponent.hpp
+++ Game/include/Components/HUDComponent.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <NFSEngine.h>
+#include <glm/ext/vector_float3.hpp>
+#include <vector>
 #include "Aura/AuraManager.hpp"
+#include "Components/Transform.hpp"
+#include "Core/Audio/AudioEngine.hpp"
 #include "Managers/ScoreManager.hpp"
 #include "Managers/LivesManager.hpp"
+#include "Renderer/Texture.hpp"
+#include "UI/UIComponents.hpp"
+#include "UI/UIFactory.hpp"
 
 class HUDComponent : public NFSEngine::Component {
 public:
@@ -32,6 +39,8 @@ private:
     NFSEngine::UIObject* m_ScoreLabel = nullptr;
     NFSEngine::UIObject* m_AuraLabel = nullptr;
     std::vector<NFSEngine::UIObject*> m_Hearts;
+    std::vector<NFSEngine::UIObject*> m_FullNotes;
+    size_t m_NumberOfNotes = 9;
 
     size_t m_AuraEventId = 0;
     size_t m_ScoreEventId = 0;
@@ -96,18 +105,83 @@ protected:
 
 private:
     void InitScoreUI() {
-        NFSEngine::UI::LabelParameters labelParams;
-        labelParams.position = glm::vec3(1500, 100, 2.0f);
-        labelParams.text = "SCORE: 0";
-        labelParams.color = glm::vec4(1.0f);
-        m_ScoreLabel = &NFSEngine::UI::Label(*m_Canvas, labelParams);
+        // NFSEngine::UI::LabelParameters labelParams;
+        // labelParams.position = glm::vec3(1500, 100, 2.0f);
+        // labelParams.text = "SCORE: 0";
+        // labelParams.color = glm::vec4(1.0f);
+        // m_ScoreLabel = &NFSEngine::UI::Label(*m_Canvas, labelParams);
 
-        NFSEngine::UI::ImageParameters scoreBgParams;
-        scoreBgParams.position = glm::vec3(1500, 100, 1.0f);
-        scoreBgParams.width = 400;
-        scoreBgParams.height = 100;
-        scoreBgParams.color = glm::vec4(0.0f, 0.0f, 0.0f, 0.5f);
-        NFSEngine::UI::Image(*m_Canvas, scoreBgParams);
+        // NFSEngine::UI::ImageParameters scoreBgParams;
+        // scoreBgParams.position = glm::vec3(1500, 100, 1.0f);
+        // scoreBgParams.width = 400;
+        // scoreBgParams.height = 100;
+        // scoreBgParams.color = glm::vec4(0.0f, 0.0f, 0.0f, 0.5f);
+        // NFSEngine::UI::Image(*m_Canvas, scoreBgParams);
+
+        float originalWidth = 462;
+        float originalHeight = 591;
+        float aspectRatio = originalWidth / originalHeight;
+
+        float noteHeight = 100;
+        float noteWidth = noteHeight * aspectRatio;
+        float targetWidth = 500;
+        glm::vec3 firstPosition = { (1920 - targetWidth) / 2.0f, 75.0f, 1.0f };
+
+        float stepX = (targetWidth - noteWidth) / (m_NumberOfNotes - 1);
+        NFSEngine::TextureParameters texParam;
+        texParam.WrapS = NFSEngine::TextureWrap::Clamp;
+        texParam.WrapT = NFSEngine::TextureWrap::Clamp;
+        auto bgTex = NFSEngine::Texture::Create("assets/textures/ui/notes/note_ui_background.png", texParam);
+        auto emptyTex = NFSEngine::Texture::Create("assets/textures/ui/notes/note_ui_empty_v2.png", texParam);
+        auto fullTex = NFSEngine::Texture::Create("assets/textures/ui/notes/note_ui_full.png", texParam);
+        auto fullExtraTex = NFSEngine::Texture::Create("assets/textures/ui/notes/note_ui_full_extra.png", texParam);
+
+        for (int i = 0; i < m_NumberOfNotes; ++i) {
+            NFSEngine::UI::ImageParameters noteParameters;
+
+            glm::vec3 currentPosition = firstPosition;
+            currentPosition.x += i * stepX;
+
+            noteParameters.position = currentPosition;
+            noteParameters.width = noteWidth;
+            noteParameters.height = noteHeight;
+            noteParameters.color = glm::vec4(1, 1, 1, 1.0f);
+            noteParameters.texture = bgTex;
+
+            NFSEngine::UI::Image(*m_Canvas, noteParameters);
+        }
+        for (int i = 0; i < m_NumberOfNotes; ++i) {
+            NFSEngine::UI::ImageParameters noteParameters;
+
+            glm::vec3 currentPosition = firstPosition;
+            currentPosition.x += i * stepX;
+
+            noteParameters.position = currentPosition + glm::vec3(0, 0, 1 + (1 * i));
+            noteParameters.width = noteWidth;
+            noteParameters.height = noteHeight;
+            noteParameters.color = glm::vec4(1, 1, 1, 1.0f);
+            noteParameters.texture = emptyTex;
+
+            NFSEngine::UI::Image(*m_Canvas, noteParameters);
+        }
+        m_FullNotes.reserve(m_NumberOfNotes);
+        for (int i = 0; i < m_NumberOfNotes; ++i) {
+            NFSEngine::UI::ImageParameters noteParameters;
+
+            glm::vec3 currentPosition = firstPosition;
+            currentPosition.x += i * stepX;
+
+            noteParameters.position = currentPosition + glm::vec3(0, 0, 2 + (1 * i));
+            noteParameters.width = noteWidth;
+            noteParameters.height = noteHeight;
+            noteParameters.color = glm::vec4(1, 1, 1, 0.0f);
+            if (i % 3 == 2)
+                noteParameters.texture = fullExtraTex;
+            else
+                noteParameters.texture = fullTex;
+
+            m_FullNotes.push_back(&NFSEngine::UI::Image(*m_Canvas, noteParameters));
+        }
     }
     void InitHeartsUI() {
         constexpr int k_MaxLives = 3; // XD?
@@ -135,22 +209,40 @@ private:
     }
     void InitAuraUI() {
         NFSEngine::UI::ImageParameters auraBgParams;
-        auraBgParams.position = glm::vec3(960, 100, 1.0f);
+        auraBgParams.position = glm::vec3(960, 1000, 1.0f);
         auraBgParams.width = 400;
         auraBgParams.height = 100;
         auraBgParams.color = glm::vec4(0.0f, 0.0f, 0.0f, 0.5f);
         NFSEngine::UI::Image(*m_Canvas, auraBgParams);
 
         NFSEngine::UI::LabelParameters labelParams;
-        labelParams.position = glm::vec3(960, 100, 2.0f);
+        labelParams.position = glm::vec3(960, 1000, 2.0f);
         labelParams.text = "AURA: NONE";
         labelParams.color = glm::vec4(1.0f);
         m_AuraLabel = &NFSEngine::UI::Label(*m_Canvas, labelParams);
     }
 
     void UpdateScoreText(int newScore) {
-        if (m_ScoreLabel && m_ScoreLabel->HasComponent<NFSEngine::TextComponent>()) {
-            m_ScoreLabel->GetComponent<NFSEngine::TextComponent>()->TextString = "SCORE: " + std::to_string(newScore);
+        // if (m_ScoreLabel && m_ScoreLabel->HasComponent<NFSEngine::TextComponent>()) {
+        //     m_ScoreLabel->GetComponent<NFSEngine::TextComponent>()->TextString = "SCORE: " + std::to_string(newScore);
+        // }
+        if (!m_FullNotes.empty()) {
+            int allCoins = 132;
+            float note = 132 / m_NumberOfNotes;
+            float howMuchCompleted = newScore / note;
+            float lastNote = std::fmod(howMuchCompleted, 1.0f);
+
+            if ((int)howMuchCompleted >= m_NumberOfNotes) {
+                for (int i = 0; i < m_NumberOfNotes; i++) {
+                    m_FullNotes[i]->GetComponent<NFSEngine::ImageComponent>()->Color.a = 1;
+                }
+            } else {
+                for (int i = 0; i < howMuchCompleted; i++) {
+                    m_FullNotes[i]->GetComponent<NFSEngine::ImageComponent>()->Color.a = 1;
+                }
+                if ((int)howMuchCompleted < m_NumberOfNotes)
+                    m_FullNotes[(int)howMuchCompleted]->GetComponent<NFSEngine::ImageComponent>()->Color.a = lastNote;
+            }
         }
     }
     void UpdateHeartVisuals(int currentLives) {
